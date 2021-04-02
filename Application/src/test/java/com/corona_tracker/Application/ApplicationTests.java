2c2de2ed4f2152443746ab0f5804341798ab1740
lemair <blacklemair@gmail.com>
2021-04-02 15:24:21 +0300

    Corona-App
    Spring-Boot App

---
+++ Application/src/test/java/com/corona_tracker/Application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corona_tracker.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
